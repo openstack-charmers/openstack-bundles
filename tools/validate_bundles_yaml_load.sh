a0885fa5888b2b7ca0a1adf6cca82b10b941b03e
Frode Nordahl <frode.nordahl@canonical.com>
2020-05-26 13:20:54 +0200

    Use ruamel.yaml for updating stable bundle versions
    The round-trip mode in ruamel.yaml preserves human readable
    formatting and does not expand variables.
    
    Handle bundles using both `services` and `applications` for
    applications.
    
    Also update the simple bundle parser validation to use ruamel.yaml
    to save number of dependencies.

--- tools/validate_bundles_yaml_load.sh
+++ tools/validate_bundles_yaml_load.sh
@@ -3,5 +3,5 @@
 # Basic yaml syntax check
 bundles=$(find . -name "bundle*.yaml")
 for bundle in $bundles; do
-    /usr/bin/env python -c 'import yaml,sys;yaml.safe_load(sys.stdin)' < $bundle
+    /usr/bin/env python -c 'import sys;from ruamel.yaml import YAML;YAML(typ="safe").load(sys.stdin)' < $bundle
 done
