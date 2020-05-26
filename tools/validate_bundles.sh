f49e47be3c214220dd190e8e49ec2617d6e03091
Frode Nordahl <frode.nordahl@canonical.com>
2020-05-26 13:19:01 +0200

    Use juju to validate bundles, add traivs build matrix
    Update to use python3.
    
    Remove use of juju-deployer add missing dependencies.
    
    Remove lint of neutron tools as they should be linted in o-c-t.

--- tools/validate_bundles.sh
+++ tools/validate_bundles.sh
@@ -2,9 +2,8 @@
 # Synthetically validate bundle for yaml and Juju syntax
 #
 # NOTE(beisner): This will fail if a model and controller are not active.
-#                https://bugs.launchpad.net/bugs/1679425)
 
 bundles=$(find . -name "bundle*.yaml")
 for bundle in $bundles; do
-    juju-deployer -c $bundle -d -b
+    juju deploy --dry-run $bundle
 done
